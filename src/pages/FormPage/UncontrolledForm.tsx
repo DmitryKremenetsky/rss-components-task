ad978fe3010a6b7827264df030d8556ef4294c50
Dmitry Kremenetsky <dmitry.kremenetsky1@gmail.com>
2023-03-28 12:33:06 +0300

    fix: fix Uncontrolled

--- src/pages/FormPage/UncontrolledForm.tsx
+++ src/pages/FormPage/UncontrolledForm.tsx
@@ -4,6 +4,6 @@ import Form from '../../components/form/form';
 
 export default class FormPage extends React.Component {
   render() {
-    return <Form />;
+    return <Form nameInput={undefined} dateInput={undefined} colorSelect={undefined} />;
   }
 }
