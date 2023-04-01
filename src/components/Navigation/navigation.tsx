adf51dcf16e16d62e27099f85d023055565b1957
Dmitry Kremenetsky <dmitry.kremenetsky1@gmail.com>
2023-04-01 12:59:42 +0300

    feat: rework on react hook navigations

--- src/components/Navigation/navigation.tsx
+++ src/components/Navigation/navigation.tsx
@@ -1,37 +1,47 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState, useEffect } from 'react';
+import { Link, useLocation } from 'react-router-dom';
 import './navigation.css';
 import SearchBar from '../searchBar/searchBar';
 
-export default class Navigation extends React.Component {
-  render(): JSX.Element {
-    return (
-      <header>
-        <div>
-          <h2>
-            {window.location.pathname === '/'
-              ? 'Home'
-              : window.location.pathname === '/about'
-              ? 'About'
-              : window.location.pathname === '/form'
-              ? 'Form'
-              : 'Error'}{' '}
-            page
-          </h2>
-        </div>
-        <SearchBar />
-        <div className="link-container">
-          <Link className="link" to={'/'}>
-            Home
-          </Link>
-          <Link className="link" to={'/about'}>
-            About us
-          </Link>
-          <Link className="link" to={'/form'}>
-            From Page
-          </Link>
-        </div>
-      </header>
-    );
-  }
-}
+const Navigation = () => {
+  const location = useLocation();
+  const [pageTitle, setPageTitle] = useState('');
+
+  useEffect(() => {
+    switch (location.pathname) {
+      case '/':
+        setPageTitle('Home');
+        break;
+      case '/about':
+        setPageTitle('About');
+        break;
+      case '/form':
+        setPageTitle('Form');
+        break;
+      default:
+        setPageTitle('Error');
+    }
+  }, [location.pathname]);
+
+  return (
+    <header>
+      <div>
+        <h2>{pageTitle} page</h2>
+      </div>
+      <SearchBar />
+      <div className="link-container">
+        <Link className="link" to="/">
+          Home
+        </Link>
+        <Link className="link" to="/about">
+          About us
+        </Link>
+        <Link className="link" to="/form">
+          From Page
+        </Link>
+      </div>
+    </header>
+  );
+};
+
+export default Navigation;
