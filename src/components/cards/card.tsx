dc5ad571bdc2b47eaf8840e204bc2a2f0953e5b4
Dmitry Kremenetsky <dmitry.kremenetsky1@gmail.com>
2023-04-08 14:54:37 +0300

    feat: add search value

--- src/components/cards/card.tsx
+++ src/components/cards/card.tsx
@@ -1,24 +1,36 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './card.css';
 import useCharacters from './useCharacters';
 
-const Card = () => {
+interface CardProps {
+  searchTerm: string;
+}
+
+const Card = ({ searchTerm }: CardProps) => {
   const characters = useCharacters();
+  const [filteredCharacters, setFilteredCharacters] = useState(characters);
+  useEffect(() => {
+    setFilteredCharacters(
+      characters.filter((item) => item.name.toLowerCase().includes(searchTerm?.toLowerCase()))
+    );
+  }, [characters, searchTerm]);
 
   return (
     <div className="card-wrapper">
-      {characters.map((item) => (
-        <div key={item.id} className="card-user">
-          <div className="card">
-            <img src={item.image} alt="" />
-            <div className="card-body">
-              <h5 className="card-title">{item.name}</h5>
-              <p>{item.species}</p>
-              <p>{item.location.name}</p>
+      <div className="card-wrapper">
+        {filteredCharacters.map((item) => (
+          <div key={item.id} className="card-user">
+            <div className="card">
+              <img src={item.image} alt="" />
+              <div className="card-body">
+                <h5 className="card-title">{item.name}</h5>
+                <p>{item.species}</p>
+                <p>{item.location.name}</p>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
     </div>
   );
 };
--- src/components/searchBar/searchBar.tsx
+++ src/components/searchBar/searchBar.tsx
@@ -1,23 +1,29 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import './searchBar.css';
+import Card from '../cards/card';
 
 const SearchBar = () => {
   const [searchTerm, setSearchTerm] = useState<string>(() => {
     return localStorage.getItem('searchTerm') || '';
   });
 
-  useEffect(() => {
-    localStorage.setItem('searchTerm', searchTerm);
-  }, [searchTerm]);
+  const [isSearchClicked, setIsSearchClicked] = useState(false);
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     setSearchTerm(event.target.value);
   };
 
+  const handleSearchClick = () => {
+    setIsSearchClicked(true);
+  };
+
   return (
     <div className="search">
       <input className="search-term" type="text" value={searchTerm} onChange={handleChange} />
-      <button className="search-btn">Search</button>
+      <button className="search-btn" onClick={handleSearchClick}>
+        Search
+      </button>
+      {isSearchClicked && <Card searchTerm={searchTerm} />}
     </div>
   );
 };
